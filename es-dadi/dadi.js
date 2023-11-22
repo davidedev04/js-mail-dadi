eff15d20ae42073564810f6a59d0bccfe4785f13
Davide Prencipe <davidedev04@gmail.com>
2023-11-22 22:17:19 +0100

    esercizio finito con spiegazioni

--- es-dadi/dadi.js
+++ es-dadi/dadi.js
@@ -11,10 +11,22 @@ buttonGen.addEventListener("click",
 
         let number = Math.floor(Math.random() * 6) + 1;
         console.log(number);
+        document.getElementById("number_us").innerHTML = number;
 
         // generiamo un numero random da 1 a 6 al computer
 
         let numberComputer = Math.floor(Math.random() * 6) + 1;
         console.log(numberComputer);
+        document.getElementById("number_pc").innerHTML = numberComputer;
+
+        // diamo una condizione di vincita / pareggio
+        if (number < numberComputer) {
+            document.getElementById("win_lose").innerHTML = "ha vinto il computer!!!";
+        } else if (number > numberComputer) {
+            document.getElementById("win_lose").innerHTML = "hai vinto tu!!!";
+        } else {
+            document.getElementById("win_lose").innerHTML = "avete pareggiato!!!";
+        }
+
     }
 );
